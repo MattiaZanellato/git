8796320958950d66c7c7b966a1c6d5ed9fc8ee07
manuelmaniero <manuelcar1404@gmail.com>
2017-05-24 10:42:59 +0200

    Add files via upload

---
+++ ArrayListTarga.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class ArrayListTarga {
+	
+	public static void main (String[] args) 
+	{
+		Scanner in= new Scanner(System.in);
+		ArrayList<Targa> provatarghe= new ArrayList();
+		System.out.println("Quante targhe vuoi mettere al momento?");
+		int targhe=Integer.parseInt(in.nextLine());
+		for (int i=0; i<targhe; i++)
+		{
+			System.out.println("Inserisci la targa");
+			provatarghe.add(new Targa(in.nextLine()));
+		}
+		
+		for (int k=0; k<provatarghe.size();k++)
+		{
+			System.out.println(provatarghe.get(k).toString());
+		}
+	}
+}
+
